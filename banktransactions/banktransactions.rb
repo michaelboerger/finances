9727aec2f3f4a547ab51276c69733ccda582f7d1
Michael <michael.boerger@gmail.com>
2014-03-03 16:38:39 -0500

    Started project

---
+++ banktransactions/banktransactions.rb
@@ -0,0 +1,36 @@
+class BankTransaction
+  def debit?(transaction)
+    #+
+  end
+
+  def credit?(transaction)
+    #-
+  end
+
+  def summary
+    #outputted info
+  end
+end
+
+class BankAccount
+  def starting_balance(total)
+    #csv information
+  end
+
+  def ending_balance
+    #total changes to accounts
+  end
+
+  def summary
+    #displayed information
+  end
+end
+
+#open CSVs
+#put CSVs into hash
+#loop hash through debit? and credit?
+#get and store summary from BankTransaction
+
+#display starting bank accounts
+#change accounts based on debits and credits
+#display summary of changes
